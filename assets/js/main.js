de8f90edcc5b23c26575fe3f463cd43667d87f61
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2018-08-03 12:30:04 -0300

    create getAll based on brasil.io special section

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,10 @@ document.addEventListener('DOMContentLoaded', function(){
   });
 });
 
+/**
+ * Names dataset placeholder
+ * 
+ */
 var names = [
   'Jair Bolsonaro',
   'Ciro Gomes',
@@ -17,14 +21,21 @@ var names = [
   'Aécio Neves',
   'Mariana Davilla',
   'Marina da Silva',
-
-  // custons
-
+  //
   'Marco Antonio',
   'Bolsonaro',
   'Itamaraty'
 ]
 
+/**
+ * @name    search: return all elements that contains the name
+ *
+ * @params  node: HTMLBodyElement
+ *          name: String
+ *
+ * @return  [...HTMLBaseElement]
+ * 
+ */
 var search = function(node, name) {
 	var catches = [];
 	var __search = function(node) {
@@ -43,8 +54,70 @@ var search = function(node, name) {
 	return __search(node)
 }
 
+/**
+ * 
+ * Apply popover in nodes returned from search
+ * 
+ */
 names.forEach(name => {
   search(document.body, name).forEach(el => {
     el.innerHTML = el.innerHTML.replace(RegExp(name,'gi'),`<a href="#">${name}</a>`)
   })
 })
+
+/**
+ * 
+ * Cosuming API Brasil.io
+ * 
+ */
+var api = {
+  baseURL: 'https://brasil.io/api/dataset',
+  name: 'Jair Bolsonaro',
+  pessoa: {},
+  socio: {},
+  eleicoes: [],
+  filiacoes: [],
+  gastosCota: [],
+  gastos: [],
+
+  async getAll() {
+    let res = await fetch(`${this.baseURL}/documentos-brasil/documents/data?format=json&search=${this.name}`)
+    let { results } = await res.json()
+    
+    this.pessoa = results[2] // get jair messias bolsonaro
+    console.log(this.pessoa)
+
+    res = await fetch(`${this.baseURL}/socios-brasil/socios/data?format=json&nome_socio=${this.pessoa.name}`)
+    data = await res.json()
+
+    this.socio = data.results[0]
+    console.log(this.socio)
+
+    res = await fetch(`${this.baseURL}/eleicoes-brasil/candidatos/data?format=json&cpf_candidato=${this.pessoa.document}`)
+    data = await res.json()
+
+    this.eleicoes = data.results
+    console.log(...this.eleicoes)
+
+    res = await fetch(`${this.baseURL}/eleicoes-brasil/filiados/data?format=json&nome_do_filiado=${this.pessoa.name}`)
+    data = await res.json()
+
+    this.filiacoes = data.results
+    console.log(...this.filiacoes)
+
+    res = await fetch(`${this.baseURL}/gastos-deputados/cota_parlamentar/data?format=json&txtcnpjcpf=${this.pessoa.document}`)
+    data = await res.json()
+
+    this.gastosCota = data.results
+    console.log(...this.gastosCota)
+
+    res = await fetch(`${this.baseURL}/gastos-diretos/gastos/data?format=json&codigo_favorecido=${this.pessoa.document}`)
+    data = await res.json()
+
+    this.gastos = data.results
+    console.log(...this.gastos)
+  }
+}
+
+// test
+// api.getDocByName()
